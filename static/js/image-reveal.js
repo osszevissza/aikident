6b0de6b98ae0da1275ade26718c505def6512c21
osszevissza <osszevissza@posteo.com>
2026-09-06 23:48:31 +0200

    képfilter kontrasztosabb: 95% szürke ↔ teljes szín

--- static/js/image-reveal.js
+++ static/js/image-reveal.js
@@ -10,16 +10,16 @@
   const figyelendok = document.querySelectorAll('.media-motif img');
 
   for (const figyelendo of figyelendok) {
-    figyelendo.style.filter = 'grayscale(90%) sepia(10%)';
+    figyelendo.style.filter = 'grayscale(95%) sepia(10%)';
     observer.observe(figyelendo);
   }
 
   function fuggveny(entries) {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.style.filter = 'grayscale(30%) sepia(20%)';
+        entry.target.style.filter = 'grayscale(0%) sepia(0%)';
       } else {
-        entry.target.style.filter = 'grayscale(90%) sepia(10%)';
+        entry.target.style.filter = 'grayscale(95%) sepia(10%)';
       }
     });
   }
